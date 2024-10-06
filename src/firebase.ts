180fa167376a7549022f252791d57ad398c00167
Castillou <joajoa70584@gmail.com>
2024-10-07 00:58:04 +0900

    Authentication Update

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyC6INMtD2BHgsUWXB0lvvufj35bPw0Bfxc',
+	authDomain: 'switter-reloaded-9f00b.firebaseapp.com',
+	projectId: 'switter-reloaded-9f00b',
+	storageBucket: 'switter-reloaded-9f00b.appspot.com',
+	messagingSenderId: '174950776301',
+	appId: '1:174950776301:web:a31b28c66e07671eafbbe4',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
